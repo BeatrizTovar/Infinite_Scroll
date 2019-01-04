ce232cd2d5fe621b8878c7e5217c91348eed2069
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-04 13:44:27 -0800

    creating InfiniteScroll Component

---
+++ src/components/InfiniteScroll.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class InfiniteScroll extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <div></div>
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default InfiniteScroll;
